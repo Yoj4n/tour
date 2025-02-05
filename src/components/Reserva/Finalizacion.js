7e48249b9f989fd37749e93e3c600235e7ebb5e7
Yoj4n <yohanstyvenh@gmail.com>
2025-02-05 15:36:46 -0500

    OK Confirmacion falta estulos para componente Listo

--- src/components/Reserva/Finalizacion.js
+++ src/components/Reserva/Finalizacion.js
@@ -4,7 +4,9 @@ const Finalizacion = () => {
   return (
     <div className="finalizacion">
       <h2>¡Gracias por reservar con Tour El Caminante!</h2>
-      <p>Nos pondremos en contacto en las próximas 24 horas.</p>
+      <p>Si en algún momento necesitas cancelar o modificar tu reserva puedes hacerlo facilmente desd tu perfil de usuario en nuestra plataforma.
+      </p>
+      <p>En las próximas 24 horas, nuesyro equipo se pondrá en contacto contigo para brindarte más detalles y asegurarse de que tengsa todo losita para tu experiencia</p>
       <button>Finalizar</button>
     </div>
   );
