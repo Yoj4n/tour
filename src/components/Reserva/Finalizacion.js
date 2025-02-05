352feac6436e43dc0adee6be697243e3c2126c82
Yoj4n <yohanstyvenh@gmail.com>
2025-02-05 16:03:44 -0500

    Ok finalizado, pendiente la accion en el boton

--- src/components/Reserva/Finalizacion.js
+++ src/components/Reserva/Finalizacion.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import '../../styles/Reserva/Finalizacion.css';
+import comprobadoImg from "../../images/comprobado.png";
+
+
 const Finalizacion = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="finalizacion">
-      <h2>¡Gracias por reservar con Tour El Caminante!</h2>
-      <p>Si en algún momento necesitas cancelar o modificar tu reserva puedes hacerlo facilmente desd tu perfil de usuario en nuestra plataforma.
+      <img src={comprobadoImg} />
+      <h2 className="finalizacion-titulo">¡Gracias por reservar con Tour El Caminante!</h2>
+      <p className="finalizacion-parrafo">
+        Si en algún momento necesitas cancelar o modificar tu reserva, puedes hacerlo fácilmente desde tu perfil de usuario en nuestra plataforma.
       </p>
-      <p>En las próximas 24 horas, nuesyro equipo se pondrá en contacto contigo para brindarte más detalles y asegurarse de que tengsa todo losita para tu experiencia</p>
-      <button>Finalizar</button>
+      <p className="finalizacion-parrafo" >
+        En las próximas 24 horas, nuestro equipo se pondrá en contacto contigo para brindarte más detalles y asegurarse de que tengas todo listo para tu experiencia.
+      </p>
+      <button onClick={() => navigate("/") } className="btn-finalizarReserva">Finalizar</button>
     </div>
   );
 };
