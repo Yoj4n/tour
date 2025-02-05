8cadd5b638986cf3992d625a886c5d3fb6ccfeac
Yoj4n <yohanstyvenh@gmail.com>
2025-02-05 12:17:55 -0500

    Ok Step

--- src/components/Reserva/Step.js
+++ src/components/Reserva/Step.js
@@ -6,17 +6,17 @@ const Step = ({ paso }) => {
   return (
     <div className="step-container">
       <div className={`step ${paso === 1 ? "active" : ""}`}>
-        <div className="circle">{paso >= 1 ? "1" : ""}</div>
+        <div className="circle">1</div>
         <p>DATOS PERSONALES</p>
       </div>
       <div className={`line ${paso >= 2 ? "active" : ""}`}></div>
       <div className={`step ${paso === 2 ? "active" : ""}`}>
-        <div className="circle">{paso >= 2 ? "2" : ""}</div>
+        <div className="circle">2</div>
         <p>CONFIRMACIÓN</p>
       </div>
       <div className={`line ${paso >= 3 ? "active" : ""}`}></div>
       <div className={`step ${paso === 3 ? "active" : ""}`}>
-        <div className="circle">{paso >= 3 ? "3" : ""}</div>
+        <div className="circle">3</div>
         <p>LISTO</p>
       </div>
     </div>
